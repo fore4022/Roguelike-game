ee724b17036eaf0de455d3c146f48fc349179ea5
fore4022 <s23009@gsm.hs.kr>
2024-02-29 20:31:33 +0900

    2024-2-29-2

--- roguelike-game/Assets/Scripts/Skill/Active/Wavelength.cs
+++ roguelike-game/Assets/Scripts/Skill/Active/Wavelength.cs
@@ -1,7 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 public class Wavelength : Base_Skill
 {
     public Animator anime;
+    private int projectileSpeed;
+    protected override void init()
+    {
+        transform.position = Managers.Game.player.gameObject.transform.position;
+        //anime.play(?)
+        projectileSpeed = 12;
+    }
+    protected override void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Monster"))
+        {
+            collision.GetComponent<Monster_Controller>().attacked(skill.skillDamage);
+        }
+    }
+    private void OnBecameInvisible() { Managers.Game.objectPool.disableObject(this.GetType().Name, this.gameObject); }
 }
