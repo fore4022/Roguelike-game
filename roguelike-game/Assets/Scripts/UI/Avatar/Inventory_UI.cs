77fd0c3d1969039be602f9c17f79617916d04a9b
fore4022 <s23009@gsm.hs.kr>
2024-05-18 16:36:33 +0900

    2024-5-18-2

--- roguelike-game/Assets/Scripts/UI/Avatar/Inventory_UI.cs
+++ roguelike-game/Assets/Scripts/UI/Avatar/Inventory_UI.cs
@@ -5,14 +5,16 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
 using UnityEngine.SceneManagement;
-using System.Runtime.CompilerServices;
-using UnityEditor.Experimental.GraphView;
 public class Inventory_UI : UI_Scene
 {
     public SwipeMenu_UI swipeMenu;
 
     private Scrollbar verticalScrollBar;
     private RectTransform content;
+    private Transform pos;
+
+    private Vector2 enterPoint;
+    private Vector2 direction;
 
     enum Buttons
     {
@@ -23,59 +25,75 @@ public class Inventory_UI : UI_Scene
         Panel,
         Panel1,
         Panel2,
-        ScrollView
+        ScrollView//scroll rect child
     }
-    enum ScrollBars
+    enum ScrollRects
     {
-        ScrollBarVertical
+        ScrollRect
     }
     private void Start() 
     {
+        if (SceneManager.GetActiveScene().name == "Main") { pos = GameObject.Find($"{this.GetType().Name.Replace("_UI", "")}" + "Page").transform; }
+        else { }
+
         init();
 
-        if (SceneManager.GetActiveScene().name == "Main")
-        {
-            Transform pos = GameObject.Find($"{this.GetType().Name.Replace("_UI", "")}" + "Page").transform;
+        this.gameObject.transform.SetParent(pos);
+        RectTransform rectTransform = this.gameObject.GetComponent<RectTransform>();
 
-            this.gameObject.transform.SetParent(pos);
-            RectTransform rectTransform = this.gameObject.GetComponent<RectTransform>();
+        rectTransform.sizeDelta = pos.GetComponentInParent<RectTransform>().rect.size;
+        rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+        rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+        rectTransform.anchoredPosition = Vector2.zero;
 
-            rectTransform.sizeDelta = pos.GetComponentInParent<RectTransform>().rect.size;
-            rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
-            rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
-            rectTransform.anchoredPosition = Vector2.zero;
-
-            swipeMenu = FindObjectOfType<SwipeMenu_UI>().GetComponent<SwipeMenu_UI>();
-        }
+        swipeMenu = FindObjectOfType<SwipeMenu_UI>().GetComponent<SwipeMenu_UI>();
     }
     protected override void init()
     {
         base.init();
         bind<Button>(typeof(Buttons));
         bind<Image>(typeof(Images));
-        bind<Scrollbar>(typeof(ScrollBars));
+        bind<ScrollRect>(typeof(ScrollRects));
 
         GameObject scrollView = get<Image>((int)Images.ScrollView).gameObject;
+        //get<ScrollRect>()
 
-        verticalScrollBar = get<Scrollbar>((int)ScrollBars.ScrollBarVertical);
         content = FindChild<RectTransform>(this.gameObject, "Content", true);
 
-        UI_EventHandler evtHandle = FindParent<UI_EventHandler>(this.gameObject);
+        UI_EventHandler evtHandle1 = FindParent<UI_EventHandler>(pos.gameObject);
 
         AddUIEvent(scrollView, (PointerEventData data) =>
         {
-            evtHandle.OnBeginDragHandler.Invoke(data);
+            if (SceneManager.GetActiveScene().name == "Main") { evtHandle1.OnBeginDragHandler.Invoke(data); }
+#if UNITY_EDITOR
+            enterPoint = Input.mousePosition;
+#endif
+
+#if UNITY_ANDROID
+            enterPoint = Input.GetTouch(0).position;
+#endif
         }, Define.UIEvent.BeginDrag);
         AddUIEvent(scrollView, (PointerEventData data) =>
         {
-            evtHandle.OnDragHandler.Invoke(data);
+            if (SceneManager.GetActiveScene().name == "Main") { evtHandle1.OnDragHandler.Invoke(data); }
+#if UNITY_EDITOR
+            direction = (Vector2)Input.mousePosition - enterPoint;
+#endif
+
+#if UNITY_ANDROID
+            direction = Input.GetTouch(0).position - enterPoint;
+#endif
+            if(Managers.Game.c / 4 + (Managers.Game.c % 4 > 0 ? 1 : 0) > 5)
+            {
+                if (Mathf.Abs(direction.y) > Mathf.Abs(direction.x)) { Scroll(); }
+            }
         }, Define.UIEvent.Drag);
         AddUIEvent(scrollView, (PointerEventData data) =>
         {
-            evtHandle.OnEndDragHandler.Invoke(data);
+            if (SceneManager.GetActiveScene().name == "Main") { evtHandle1.OnEndDragHandler.Invoke(data); }
         }, Define.UIEvent.EndDrag);
 
-        evtHandle = FindParent<UI_EventHandler>(content.gameObject);
+        UI_EventHandler evtHandle2 = FindParent<UI_EventHandler>(content.gameObject);
 
         for(int h = 0; h < Mathf.Max(Managers.Game.c / 4 + (Managers.Game.c % 4 > 0 ? 1 : 0), 5); h++)//
         {
@@ -96,17 +114,22 @@ public class Inventory_UI : UI_Scene
                 }, Define.UIEvent.Click);
                 AddUIEvent(go, (PointerEventData data) =>
                 {
-                    evtHandle.OnBeginDragHandler.Invoke(data);
+                    evtHandle2.OnBeginDragHandler.Invoke(data);
                 }, Define.UIEvent.BeginDrag);
                 AddUIEvent(go, (PointerEventData data) =>
                 {
-                    evtHandle.OnDragHandler.Invoke(data);
+                    evtHandle2.OnDragHandler.Invoke(data);
                 }, Define.UIEvent.Drag);
                 AddUIEvent(go, (PointerEventData data) =>
                 {
-                    evtHandle.OnEndDragHandler.Invoke(data);
+                    evtHandle2.OnEndDragHandler.Invoke(data);
                 }, Define.UIEvent.EndDrag);
             }
+        }
+
+        if(Managers.Game.c / 4 + (Managers.Game.c % 4 > 0 ? 1 : 0) > 5)
+        { 
+            
         }
     }
     private void Scroll()
