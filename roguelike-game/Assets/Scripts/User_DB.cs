f1fa391ebe107cc4a62563462bb20763b4c55d38
fore4022 <s23009@gsm.hs.kr>
2024-05-24 20:47:17 +0900

    2024-5-24-2

--- roguelike-game/Assets/Scripts/User_DB.cs
+++ roguelike-game/Assets/Scripts/User_DB.cs
@@ -3,11 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Data;
 using Mono.Data.Sqlite;
+using System;
 public class User_DB : MonoBehaviour
 {
+    [SerializeField]
+    private string inventoryDBName = "/inventory.db";
+    [SerializeField]
+    private string inventoryTableName = "inventory";
+
+    [SerializeField]
+    private string userDBName = "/user.db";
+    [SerializeField]
+    private string userTableName = "inventory";
+
+    public IDataReader inventoryReader;
+    public IDataReader userReader;
+
+    private string pathString = "URI=file" + Application.streamingAssetsPath;
+
     private void Start()
     {
-        string dbname = "/inventory.db";
+        IDbConnection Connection = new SqliteConnection(pathString);
+
+        Connection.Open();
+
+        IDbCommand Command = Connection.CreateCommand();
+        Command.CommandText = "SELECT * FROM " + inventoryTableName;
+        inventoryReader = Command.ExecuteReader();
+
+        inventoryReader.Close();//
+
+        Connection = new SqliteConnection(pathString);
+
+        Connection.Open();
+
+        Command = Connection.CreateCommand();
+        Command.CommandText = "SELECT * FROM " + userTableName;
+        userReader = Command.ExecuteReader();
+
+        userReader.Close();
+    }
+}
+/*
+ string dbname = "/inventory.db";
         string connectionString = "URI=file:" + Application.streamingAssetsPath + dbname;
         string tableName = "inventory";
 
@@ -18,14 +56,14 @@ public class User_DB : MonoBehaviour
         dbCommand.CommandText = "SELECT * FROM " + tableName;
         IDataReader dataReader = dbCommand.ExecuteReader();
 
+        //FieldCount
         while(dataReader.Read())
         {
             string id = dataReader.GetString(1);
-            int count = dataReader.GetInt32(2); 
+            long count = dataReader.GetInt64(2);
 
-            Debug.Log(id + ":" + count);
+            Debug.Log(id + " : " + count);
         }
 
         dataReader.Close();
-    }
-}
+ */
